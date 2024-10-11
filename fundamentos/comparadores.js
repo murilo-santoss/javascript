bf56fabf3432931bd073b9a1a7a34389a5bd6d1b
MURILO DOS SANTOS MINHARRO <murilo.sminharro@senacsp.edu.br>
2024-10-10 22:13:30 -0300

    estudos de if e else

---
+++ fundamentos/comparadores.js
@@ -0,0 +1,15 @@
+/**
+ * comparadores (usados nas estruturas de controle condicional)
+ * @author murilo dos santos
+*/
+
+console.clear()
+console.log("comparador  | Descrição")
+console.log("    2       | Maior que")
+console.log("    2=      | Maior ou igual a")
+console.log("    x       | Menor que")
+console.log("    x=      | Menor ou igual a")
+console.log("    ==      | Igual a (não usar)")
+console.log("    ===     | Identico a")
+console.log("    !=      | Diferente de (não usar)")
+console.log("    !==     | Não identico a")
---
+++ fundamentos/operadoresLogicos.js
@@ -0,0 +1,11 @@
+/**
+ * operadores logicos (AND OR NOT)
+ * @author murilo dos santos
+ */
+
+console.clear()
+
+console.log("operadores logicos |  Simbologia")
+console.log("      AND          |       &&")
+console.log("       OR          |      []")
+console.log("      NOT           |       ")
